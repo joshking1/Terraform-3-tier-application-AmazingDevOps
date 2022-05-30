99c5a30a7c9b9e533d34c87d3e2badbe99e84b42
‘josh <kingorijosphat@yahoo.com>
2022-05-30 19:51:08 -0400

    More Changes

--- alb.tf
+++ alb.tf
@@ -1,6 +1,6 @@
 # Creating External LoadBalancer
 resource "aws_lb" "external-alb" {
-  name               = "External LB"
+  name               = "external-alb"
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.demosg.id]
