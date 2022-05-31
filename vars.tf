67596662478b9d4c5b1e0896a594826f3f1c0005
‘josh <kingorijosphat@yahoo.com>
2022-05-30 20:38:43 -0400

    CIDR

--- vars.tf
+++ vars.tf
@@ -7,7 +7,7 @@ variable "subnet_cidr" {
   default = "10.0.1.0/24"
 }
 # Defining CIDR Block for 2nd Subnet
-variable "subnet1_cidr" {
+variable "subnet2_cidr" {
   default = "10.0.2.0/24"
 }
 # Defining CIDR Block for 3rd Subnet
