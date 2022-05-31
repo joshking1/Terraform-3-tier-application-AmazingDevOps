116d66189d6f1ee18ecfa9f057df8a2cf9159df5
‘josh <kingorijosphat@yahoo.com>
2022-05-30 20:50:47 -0400

    Modification

--- ec2.tf
+++ ec2.tf
@@ -21,7 +21,7 @@ resource "aws_instance" "demoinstance1" {
   count                       = 1
   key_name                    = "Apollo"
   vpc_security_group_ids      = ["${aws_security_group.demosg.id}"]
-  subnet_id                   = "${aws_subnet.demoinstance.id}"
+  subnet_id                   = "${aws_subnet.public-subnet-1.id}"
   associate_public_ip_address = true
   user_data                   = "${file("data.sh")}"
 
