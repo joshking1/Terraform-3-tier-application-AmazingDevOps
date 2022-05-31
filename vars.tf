8a7541fcd9364314149de3cf7b6be51f2c586b06
‘josh <kingorijosphat@yahoo.com>
2022-05-30 20:41:09 -0400

    CIDR

--- vars.tf
+++ vars.tf
@@ -3,7 +3,7 @@ variable "vpc_cidr" {
   default = "10.0.0.0/16"
 }
 # Defining CIDR Block for 1st Subnet
-variable "subnet_cidr" {
+variable "subnet1_cidr" {
   default = "10.0.1.0/24"
 }
 # Defining CIDR Block for 2nd Subnet
